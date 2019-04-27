b026b67c4fbf7e70db1078df155bff0dd6492ce9
MSI\Wu <zhangwu@sheridancollege.ca>
2019-04-27 14:35:56 -0400

    Fmod Sample

---
+++ OdinTest/Assets/RootGame/Scripts/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [Button]
+    public void AndyTheme()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Test");
+    }
+
+}
